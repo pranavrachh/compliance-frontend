df3659a67933b37efc6b74dd9a3b001608861263
Pranav Rachh <info@rightflowindia.com>
2025-06-08 21:34:28 +0530

    fix: remove incorrect rollup input config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  publicDir: 'public',
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
+    outDir: 'build'
   },
   server: {
     open: true
